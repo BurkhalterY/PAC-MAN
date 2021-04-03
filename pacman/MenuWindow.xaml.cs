a12249d61ed17800351f1003cf177012af8f6a2e
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-04-03 17:30:32 +0200

    Add menu

---
+++ pacman/MenuWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pacman
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDebug(object sender, RoutedEventArgs e)
+        {
+            Game.debug = true;
+            new MainWindow().Show();
+            Close();
+        }
+
+        private void Btn1Player(object sender, RoutedEventArgs e)
+        {
+            Game.debug = false;
+            new MainWindow().Show();
+            Close();
+        }
+
+        private void BtnEditor(object sender, RoutedEventArgs e)
+        {
+            new EditorWindow().Show();
+            Close();
+        }
+    }
+}
